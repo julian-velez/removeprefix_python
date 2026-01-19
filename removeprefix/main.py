82aa51756ed03387c8e190e1ed401c74da3a9d71
julian-velez <60607869+julian-velez@users.noreply.github.com>
2026-01-19 08:54:10 -0500

    removeprefix_python
    removeprefix_python

---
+++ removeprefix/main.py
@@ -0,0 +1,9 @@
+# Se crea una variable llamada 'texto' que almacena una cadena de texto
+texto = "julian Velez G"
+
+# Se usa el método removeprefix() para eliminar el prefijo "julian"
+# Solo se elimina si la cadena comienza exactamente con ese texto
+resultado = texto.removeprefix("julian")
+
+# Se imprime el resultado final en la consola
+print(resultado)
